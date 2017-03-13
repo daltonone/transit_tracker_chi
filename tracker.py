4034177bc330aa75527afaf0a2b828691303506c
Adrian <adrian@dalton.one>
2017-03-13 15:21:26 -0500

    Tracker v1.0

--- tracker.py
+++ tracker.py
@@ -3,6 +3,7 @@
 import re
 import requests
 import json
+import math
 from bs4 import BeautifulSoup
 
 location 	= raw_input("Enter your address [Blank to use ip address] ")
@@ -21,8 +22,6 @@ else:
 	lat	= loc['lat']
 	lon	= loc['lng']
 
-print str(lat)+' '+str(lon)
-
 trainbase	= "http://www.transitchicago.com/traintracker/popout.aspx?sid="
 trains		= [
 	{"line":["red","yellow","purple"], "stop":"Howard", "url":"40900", "lat":42.018769, "long":-87.672535},
@@ -31,21 +30,36 @@ trains		= [
         {"line":["red"], "stop":"Loyola", "url":"41300", "lat":42.001154, "long":-87.661049},
         {"line":["red"], "stop":"Granville", "url":"40760", "lat":41.993771, "long":-87.659140},
         {"line":["red"], "stop":"Thorndale", "url":"40880", "lat":41.990262, "long":-87.659054},
-	{"line":["red"], "stop":"Addison", "url":"41090"}
+	{"line":["red"], "stop":"Addison", "url":"41090", "lat":41.947522, "long":-87.653732}
     ]
 
-for stop in trains:
-    url		= trainbase+stop["url"]
-    req		= requests.get(url)
-    data	= req.text
-    soup	= BeautifulSoup(data, "lxml")
-    train	= []
-    time	= []
-    for div in soup(attrs={'class' : re.compile(r'^ttar_traindest')}):
-    	train.append(div.text.strip())
-    for div in soup(attrs={'class' : re.compile(r'^ttar_trainpred')}):
-    	time.append(div.text.strip())
-    for i, j in zip(train, time):
-    	print i+" - "+j
+distances = {}
 
-print location
+for stop in trains:
+    a = stop['lat'] - lat
+    b = stop['long'] - lon
+    c = math.sqrt((a*a) + (b*b))
+    distances[stop['url']] = c
+
+for val in sorted(distances.values())[:3]:
+    for key, dis in distances.iteritems():
+    	if dis == val:
+	    url = trainbase+key
+    	    req	= requests.get(url)
+	    data	= req.text
+	    soup	= BeautifulSoup(data, "lxml")
+	    train	= []
+	    time	= []
+	    for i in trains:
+		if i['url'] == key:
+		    print ' '
+		    print i['stop']
+		    for j in i['line']:
+			print j+' line'
+	    for div in soup(attrs={'class' : re.compile(r'^ttar_traindest')}):
+	    	train.append(div.text.strip())
+	    for div in soup(attrs={'class' : re.compile(r'^ttar_trainpred')}):
+	    	time.append(div.text.strip())
+	    for i, j in zip(train, time):
+	    	info = i+" - "+j
+		print info
